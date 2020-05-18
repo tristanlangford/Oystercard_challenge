cfc62fa62420be0d12b5cf189d3af67904fcc5c5
Tristan Langford <tristanlangford@hotmail.com>
2020-05-18 15:22:26 +0100

    added top_up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,4 +4,9 @@ attr_reader :balance
   def initialize
     @balance = 0
   end
+
+  def top_up(money)
+    @balance += money
+  end
+  
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,9 @@ describe Oystercard do
   it 'can access the  default balance of 0' do
     expect(subject.balance).to eq(0)
   end
+
+  it 'tops up the balance with the argument value' do
+    subject.top_up(20)
+    expect(subject.balance).to eq(20)
+  end 
 end
